1d469fd5bd15d569f8d8d085937530bce0f01a94
DeyanSpasovBerlin <pislpe@gmail.com>
2024-09-09 19:37:59 +0200

    Add files via upload

--- Homework12Zada4a1New.java
+++ Homework12Zada4a1New.java
@@ -6,25 +6,10 @@ public class Homework12Zada4a1New{
     //private Boolean canBuy;
     Integer timeNowNew = LocalDateTime.now().getHour();
     public void canByu() {
-        System.out.println();
-        if (timeNowNew >= 6 && timeNowNew <= 20) {
-            isEdekaOpen = true;
-        } else {
-            isEdekaOpen = false;
-        }
-        if (timeNowNew >= 7 && timeNowNew <= 21) {
-            isReweOpen = true;
-        } else {
-            isReweOpen = false;
-        }
-        if(isEdekaOpen==true && isReweOpen==true){
-            System.out.println("Сейчас откытые и Edeka и Rewe!");
-        }else if (isEdekaOpen==true && isReweOpen==false){
-            System.out.println("Сейчас откытo  Edeka!");
-        } else if (isEdekaOpen==false && isReweOpen==true) {
-            System.out.println("Сейчас откытo  Rewe!");
-        }else {
-            System.out.println("Сейчас закрытые и Edeka и Rewe!");
-        }
+        System.out.println("Сейчас "+timeNowNew+" часа.");
+        isEdekaOpen = (timeNowNew >= 6 && timeNowNew <= 20) ? true : false;
+       isReweOpen = (timeNowNew >= 7 && timeNowNew <= 21) ? true : false;
+       String result1 = (isEdekaOpen && isReweOpen) ? "Сейчас откытые и Edeka и Rewe!" : (isEdekaOpen && !isReweOpen) ? "Сейчас откытo  Edeka!" : (!isEdekaOpen && isReweOpen) ? "Сейчас откытo  Rewe!" : "Сейчас закрытые и Edeka и Rewe!";
+        System.out.println(result1);
     }
 }
--- Homework12Zada4a2New.java
+++ Homework12Zada4a2New.java
@@ -1,22 +1,12 @@
 import java.util.Scanner;
 
 public class Homework12Zada4a2New {
-    //private  Integer year;
     public void printYear(){
         Scanner reader = new Scanner(System.in);
         System.out.println("Input any year and check is it Leap: \n");
         Integer year = reader.nextInt();
         Integer flag = (year%100)%400;
-        if (year%100 == 0 ){
-            if(year%400 == 0){
-                System.out.println(" Year "+year+" is Leap");
-            } else {
-                System.out.println(" Year "+year+" is not Leap");
-            }
-        }else if((year % 4) == 0) {
-            System.out.println(" Year "+year+" is Leap");
-        } else {
-            System.out.println(" Year "+year+" is not Leap");
-        }
+            String result = ((year%100 == 0) && (year%400 == 0)) ? " Year "+year+" is Leap" : ((year % 4) == 0) ?    "Year "+year+" is Leap" : " Year "+year+" is not Leap";
+            System.out.println(result);
     }
 }
